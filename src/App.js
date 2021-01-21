1c82b8df558013b0cc3a2d6d74e93d60315e70bb
thaibm <thaibm.uet@gmail.com>
2021-01-22 00:09:26 +0700

    Fix shaking card while moving

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
-import TinderCard from './components/tinder-card/TinderCard';
+import Advanced from './components/Advanced';
 
 function App() {
-  const cards = [
-    { imgUrl: logo, name: 'Thaibm' },
-    { imgUrl: logo, name: 'Anhtl' },
-    { imgUrl: logo, name: 'Doctor Strange' },
-    { imgUrl: logo, name: 'Captain' },
-    { imgUrl: logo, name: 'Thor' },
-  ];
   return (
     <div className="App">
       <header className="App-header">
-        {cards.map((card, index) => (
-          <TinderCard key={index} className="App-card">
-            <img src={card.imgUrl} className="App-logo" alt="logo" />
-            <p>{card.name}</p>
-          </TinderCard>
-        ))}
+        <Advanced></Advanced>
       </header>
     </div>
   );
